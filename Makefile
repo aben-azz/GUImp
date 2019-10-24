298ff69822e56563154138f0d28b6f5fd80b86c5
Achraf <macadelic1030@gmail.com>
2019-10-24 12:22:17 +0200

    ajout libft, makefile/srcs/includes libui, makefile/srcs/includes guimp.

---
+++ Makefile
@@ -0,0 +1,90 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: aben-azz <aben-azz@student.s19.be>         +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/02/03 09:24:41 by aben-azz          #+#    #+#              #
+#    Updated: 2019/10/24 12:21:15 by aben-azz         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+_END			=	\x1b[0m
+_BOLD			=	\x1b[1m
+_UNDER			=	\x1b[4m
+_REV			=	\x1b[7m
+_GREY			=	\x1b[30m
+_RED			=	\x1b[31m
+_GREEN			=	\x1b[32m
+_YELLOW			=	\x1b[33m
+_BLUE			=	\x1b[34m
+_PURPLE			=	\x1b[35m
+_CYAN			=	\x1b[36m
+_WHITE			=	\x1b[37m
+_IGREY			=	\x1b[40m
+_IRED			=	\x1b[41m
+_IGREEN			=	\x1b[42m
+_IYELLOW		=	\x1b[43m
+_IBLUE			=	\x1b[44m
+_IPURPLE		=	\x1b[45m
+_ICYAN			=	\x1b[46m
+_IWHITE			=	\x1b[47m
+_MAGENTA		=	\x1b[35m
+
+NAME = GUImp
+
+MSG				=	$(_BOLD)$(_BLUE)Compiling $(NAME):$(_END)
+.PHONY: all, $(NAME), clean, fclean, re
+
+
+cc = gcc
+FLAGS = -Wall -Wextra -Werror #-g -v #-fsanitize=address
+SRC_NAME = main.c
+OBJ_PATH = ./obj/
+
+LFT_PATH = ./libft/
+LFT_NAME = libft.a
+LUI_PATH = ./libui/
+LUI_NAME = libui.a
+
+INC_PATH = ./includes
+SRC_PATH = ./srcs/
+OBJ_NAME = $(SRC_NAME:.c=.o)
+INC_FPATH = ./includes/guimp.h
+SRC = $(addprefix $(SRC_PATH),$(SRC_NAME))
+LONGEST			=	$(shell echo $(notdir $(SRC)) | tr " " "\n" | awk ' { if (\
+				length > x ) { x = length; y = $$0 } }END{ print y }' | wc -c)
+OBJ = $(addprefix $(OBJ_PATH),$(OBJ_NAME))
+INC = $(addprefix -I,$(INC_PATH))
+
+all: $(LFT_PATH)$(LFT_NAME) $(LUI_PATH)$(LUI_NAME) $(NAME)
+
+$(LFT_PATH)$(LFT_NAME):
+	@$(MAKE) -C $(LFT_PATH);
+
+$(LUI_PATH)$(LUI_NAME):
+	@$(MAKE) -C $(LUI_PATH);
+
+$(NAME): $(LIBFT_PATH)$(LIBFT_NAME) $(LIBUI_PATH)$(LIBUI_NAME) $(OBJ)
+	@$(CC) $(FLAGS) -o $(NAME) -L $(LFT_PATH) -L $(LUI_PATH) -lft -lui $^ -o $@
+	@printf "\r\033[K$(_BOLD)$(_RED)./$(NAME) is ready for use\n$(_END)"
+
+$(OBJ_PATH)%.o: $(SRC_PATH)%.c $(INC_FPATH)
+	@mkdir -p $(OBJ_PATH)
+	@$(CC) $(FLAGS) $(INC) -o $@ -c $<
+	@printf "\r\033[K$(MSG) $(_BOLD)$(_CYAN)%-$(LONGEST)s\$(_END)" $(notdir $<)
+
+clean:
+	@make -C $(LFT_PATH) clean
+	@make -C $(LUI_PATH) clean
+	@rm -rf $(OBJ_PATH)
+	@echo "$(_BOLD)$(_RED)Sucesfuly removed all objects from minishell$(_END)"
+
+fclean: clean
+	@make -C $(LFT_PATH) fclean
+	@make -C $(LUI_PATH) fclean
+	@rm -f $(NAME)
+	@echo "$(_BOLD)$(_RED)Sucessfuly removed ${NAME} from minishell$(_END)"
+
+re: fclean all
---
+++ libft/includes/libft.h
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aben-azz <aben-azz@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/04 13:56:36 by aben-azz          #+#    #+#             */
+/*   Updated: 2019/10/24 12:07:06 by aben-azz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <stdarg.h>
+# define ABS(x) ((x < 0) ? -x : x)
+# define TYPES    "cspPfFdDiIoOuUXxbvr%"
+# define LENGTH   "lhLzj"
+# define HASH    (1 << 0)
+# define ZERO    (1 << 1)
+# define SUB     (1 << 2)
+# define L_      (1 << 1) + 1
+# define LL_     (1 << 1)
+# define H_      (1 << 2) + 1
+# define HH_     (1 << 2)
+# define LU_     (1 << 3) + 1
+# define Z_      (1 << 4) + 1
+# define J_      (1 << 5) + 1
+# define MAX OPEN_MAX
+# define BUFF_SIZE 32
+# define FD_MAX OPEN_MAX
+
+typedef struct	s_gnl
+{
+	int			i;
+	int			i_read;
+	char		buff[BUFF_SIZE + 1];
+	char		*ptr_leak;
+	char		*s[MAX];
+}				t_gnl;
+
+int				get_next_line(int fd, char **line, char separator);
+void			*ft_memset(void *b, int c, size_t len);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t			ft_strlen(const char *s);
+char			*ft_strdup(const char *s1);
+char			*ft_strcpy(char *dst, const char *src);
+char			*ft_strncpy(char *dst, const char *src, size_t len);
+char			*ft_strcat(char *s1, const char *s2);
+char			*ft_strncat(char *s1, const char *s2, size_t n);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strstr(const char *haystack, const char *needle);
+char			*ft_strnstr(const char *haystack, const char *needle,
+		size_t len);
+int				ft_strcmp(const char *s1, const char *s2);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_atoi(const char *str);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
+char			*ft_strnew(size_t size);
+void			ft_strdel(char **as);
+void			ft_strclr(char *s);
+void			ft_striter(char *s, void (*f)(char *));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+char			*ft_strmap(char const *s, char (*f)(char));
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int				ft_strequ(char const *s1, char const *s2);
+int				ft_strnequ(char const *s1, char const *s2, size_t n);
+char			*ft_strsub(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s);
+char			**ft_strsplit(char const *s, char c);
+char			*ft_itoa(intmax_t n);
+int				ft_putchar(char c);
+int				ft_putstr(char const *s);
+int				ft_putendl(char const *s);
+int				ft_putnbr(int n);
+int				ft_putchar_fd(char c, int fd);
+int				ft_nputchar(char c, int n);
+int				ft_putstr_fd(char const *s, int fd);
+int				ft_putendl_fd(char const *s, int fd);
+int				ft_putnbr_fd(int n, int fd);
+char			*ft_itoa_base(intmax_t nb, int base, int uppercase);
+char			*ft_ftoa(long double d, int precision);
+char			*ft_utoa_base(uintmax_t nb,
+				unsigned base, int uppercase);
+int				ft_count_char(char c, char *str);
+int				ft_count_words(char const *s, char c);
+double			interpolate(double start, double end, double interpolat);
+
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+
+t_list			*ft_lstnew(void const *content, size_t content_size);
+void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void			ft_lstadd(t_list **alst, t_list *new);
+int				ft_lstpushback(t_list **begin, t_list *new);
+void			ft_lstrev(t_list **alst);
+int				ft_lstdestroy(t_list **lst);
+
+typedef struct	s_fmt
+{
+	int				prec;
+	int				length;
+	int				type;
+	int				field;
+	int				opt;
+	int				signe;
+	int				prefixe;
+}				t_fmt;
+typedef struct	s_ype
+{
+	int				type;
+	int				(*function)(int fd, va_list list, t_fmt *fmt);
+}				t_ype;
+int				ft_printf(const char *restrict format, ...);
+int				handle_char(int fd, va_list list, t_fmt *fmt);
+int				handle_string(int fd, va_list list, t_fmt *fmt);
+int				handle_array(int fd, va_list list, t_fmt *fmt);
+int				print_numbers(int fd, t_fmt *fmt, char *str, int len);
+int				handle_number(int fd, va_list ap, t_fmt *fmt);
+t_fmt			*get_options(char *s, va_list ap);
+char			*get_s(t_fmt *fmt, va_list ap);
+int				ft_printf(const char *format, ...);
+int				ft_dprintf(int fd, const char *format, ...);
+intmax_t		ft_abs(intmax_t nb);
+int				ft_get_color(int color1, int color2, double percentage);
+double			ft_percent(int start, int end, int curr);
+int				ft_split_count(const char **split);
+void			ft_splitdel(char **split);
+char			*ft_strcjoin(char const *s1, char const *s2, char c);
+char			**ft_strsplitstr(char const *s, char *needle);
+int				ft_count_words_str(char const *s, char *needle);
+int				ft_lastindexof(char *string, char c);
+size_t			ft_levenshtein_n(char *a, size_t length, char *b, size_t blen);
+size_t			ft_levenshtein(char *a, char *b);
+int				ft_indexof(char *string, char separator);
+char			*ft_substr(char *src, int m, int n);
+int				ft_isonly_digit(char *str);
+size_t			ft_strlen2(const char *str);
+char			**ft_splitdup(char **src);
+int				ft_min(int a, int b);
+int				ft_max(int a, int b);
+void			ft_exit(char *msg);
+int				ft_nlastindexof(char *string, char c, int n);
+int				ft_isspace(int c);
+char			*ft_strreplace(char *orig, char *rep, char *with);
+int				ft_nputchar_fd(char c, int n, int fd);
+int				ft_counti(char *string, char c, int i);
+#endif
